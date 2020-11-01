ff3c8c295af577c649442250619aba3ac837408d
Ujjwal Sharma <ryzokuken@disroot.org>
2020-11-01 19:11:42 +0530

    basic error and status reporting

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::io::prelude::*;
+use std::io::Error;
 use std::path::PathBuf;
+use std::process::Output;
 
 use colored::Colorize;
 use yaml_rust::Yaml;
@@ -45,12 +47,7 @@ fn generate_includes(includes: Vec<&str>, include_path: &PathBuf) -> String {
     contents
 }
 
-fn process_file(test_path: &PathBuf, include_path: &PathBuf, display_path: Option<&str>) {
-    println!(
-        "{} {}",
-        "RUN".to_string().yellow(),
-        display_path.unwrap_or_else(|| test_path.to_str().unwrap())
-    );
+fn process_file(test_path: &PathBuf, include_path: &PathBuf) -> Result<Output, Error> {
     let mut test_file = std::fs::File::open(test_path).unwrap();
     let mut contents = String::new();
     test_file.read_to_string(&mut contents).unwrap();
@@ -68,31 +65,46 @@ fn process_file(test_path: &PathBuf, include_path: &PathBuf, display_path: Optio
     include_contents.push_str(&contents);
     let mut final_file = tempfile::NamedTempFile::new().unwrap();
     final_file.write_all(include_contents.as_bytes()).unwrap();
-    let _node_process = std::process::Command::new("node")
+    std::process::Command::new("node")
         .arg(final_file.path())
         .output()
-        .unwrap();
 }
 
-pub fn run_test(test_path: PathBuf, include_path: PathBuf) {
+fn print_test_status(output: Output, path: &str) {
+    if output.status.success() {
+        println!("{} {}", "PASS".to_string().green(), path);
+    } else {
+        println!("{} {}", "FAIL".to_string().red(), path);
+        println!("{}", String::from_utf8(output.stderr).unwrap());
+    }
+}
+
+pub fn run_test(test_path: PathBuf, include_path: PathBuf) -> Result<(), Error> {
     if test_path.is_file() {
-        process_file(&test_path, &include_path, None);
+        let res = process_file(&test_path, &include_path);
+        if let Err(e) = res {
+            return Err(e);
+        }
+        print_test_status(res.unwrap(), test_path.to_str().unwrap());
+        Ok(())
     } else {
         for entry in walkdir::WalkDir::new(test_path.clone()) {
             let ent = entry.unwrap();
             if ent.metadata().unwrap().is_file() {
-                process_file(
-                    &ent.clone().into_path(),
-                    &include_path,
-                    Some(
-                        ent.into_path()
-                            .strip_prefix(test_path.clone())
-                            .unwrap()
-                            .to_str()
-                            .unwrap(),
-                    ),
+                let res = process_file(&ent.path().to_path_buf(), &include_path);
+                if let Err(e) = res {
+                    return Err(e);
+                }
+                print_test_status(
+                    res.unwrap(),
+                    ent.path()
+                        .strip_prefix(test_path.clone())
+                        .unwrap()
+                        .to_str()
+                        .unwrap(),
                 );
             }
         }
+        Ok(())
     }
 }
